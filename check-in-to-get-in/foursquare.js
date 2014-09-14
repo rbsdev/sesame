c61a66f37c26a2e35e38fbd05f860e885fef111a
Evandro Eisinger <evandroeisinger@gmail.com>
2014-09-13 23:37:54 -0300

    Checkin bug fix

--- check-in-to-get-in/foursquare.js
+++ check-in-to-get-in/foursquare.js
@@ -1,16 +1,16 @@
 var OAuth2 = require('oauth').OAuth2,
-	config = require('./config');
+		config = require('./config');
 
 function Foursquare() {
 
 	return new OAuth2(
 		config.client_id,
-    	config.client_secret,
-    	'https://foursquare.com',
-    	'/oauth2/authenticate',
-    	'/oauth2/access_token',
-    	null
-    );
+			config.client_secret,
+			'https://foursquare.com',
+			'/oauth2/authenticate',
+			'/oauth2/access_token',
+			null
+		);
 
 }
 

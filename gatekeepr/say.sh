4787ad12b314ff673207371d0c2a0fd7bf894c9a
Sesame Team <sesame@gruporbs.com.br>
2014-09-14 06:46:10 +0000

    Change sound API

--- gatekeepr/say.sh
+++ gatekeepr/say.sh
@@ -1,10 +1,9 @@
 #!/bin/sh
 say () {
    echo $* 
-   echo "http://translate.google.com/translate_tts?tl=pt-br&q=$*"; 
-   mplayer "http://translate.google.com/translate_tts?tl=pt-br&q=$*"; 
+   mplayer "http://api.voicerss.org/?key=50959f3a9fb04e4b9ee978fc291fa362&src=$*&hl=pt-br"
 }
 fort=$(fortune)
-all_say=$(echo  \"$fort\" | sed 'y/áÁàÀãÃâÂéÉêÊíÍóÓõÕôÔúÚçÇ/aAaAaAaAeEeEiIoOoOoOuUcC/') 
+all_say=$(echo  \"$1, bem-vindo ao nucleo digital RBS... $fort\" | sed 'y/áÁàÀãÃâÂéÉêÊíÍóÓõÕôÔúÚçÇ/aAaAaAaAeEeEiIoOoOoOuUcC/') 
 echo $all_say
 say $all_say

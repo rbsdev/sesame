f8eec11f54cd35684476fea177034d53137ba30c
Michele Silva <michele.s@icanlocalize.com>
2014-09-13 22:40:15 -0300

    Add main mercury runner

--- mercury/mercury.py
+++ mercury/mercury.py
@@ -1,1 +1,16 @@
-import 
+#!/usr/bin/env python
+
+from gatekeepr import GatekeeprClient 
+from scanner import DeviceScanner
+import logging
+
+
+def main():
+    gatekeepr_client = GatekeeprClient()
+    device_scanner = DeviceScanner()  
+    device_scanner.start(gatekeepr_client.open)
+
+
+if __name__ == '__main__':
+    logging.getLogger().setLevel(logging.INFO)
+    main()

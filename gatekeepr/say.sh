f5c6785a75db235ec2a8fa611274a3fc075dd89a
Sesame Team <sesame@gruporbs.com.br>
2014-09-14 07:08:40 +0000

    Increase volume to unbearable levels\!

--- gatekeepr/say.sh
+++ gatekeepr/say.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 say () {
    echo $* 
-   mplayer "http://api.voicerss.org/?key=50959f3a9fb04e4b9ee978fc291fa362&src=$*&hl=pt-br"
+   mplayer -volume 90 "http://api.voicerss.org/?key=50959f3a9fb04e4b9ee978fc291fa362&src=$*&hl=pt-br"
 }
 fort=$(fortune)
 all_say=$(echo  \"$1, bem-vindo ao nucleo digital RBS... $fort\" | sed 'y/áÁàÀãÃâÂéÉêÊíÍóÓõÕôÔúÚçÇ/aAaAaAaAeEeEiIoOoOoOuUcC/') 
